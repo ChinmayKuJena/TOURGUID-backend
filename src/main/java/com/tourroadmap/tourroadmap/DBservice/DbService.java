0e9ccaf58670cb8e7d9ca33f20e9b94586b8bea7
ChinmayKuJena <chinmay.jena7878@gmail.com>
2024-06-18 10:31:22 +0530

    App Name , Date & Time Added to SearchId

--- src/main/java/com/tourroadmap/tourroadmap/DBservice/DbService.java
+++ src/main/java/com/tourroadmap/tourroadmap/DBservice/DbService.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,12 +36,15 @@ public class DbService {
                 log.info("Connection null");
 
             }
-            UUID uuid = UUID.randomUUID();
-            String PREFIX = "TRV_EXP_SR_ID";
-            String searchId = PREFIX + "-" + uuid.toString();
-            System.out.println(searchId);
+            // UUID uuid = UUID.randomUUID();
+            // String PREFIX = "TRV_EXP_SR_ID";
+            // String timeStamp = new
+            // SimpleDateFormat("***yyyy**MM**dd**-HH**mm**ss***").format(new Date());
+
+            // String searchId = PREFIX + " - " + uuid.toString() + " - " + timeStamp;
+            // System.out.println(searchId);
             // placedetails(rs, placeList, UUID.randomUUID().toString());
-            placedetails(rs, placeList, searchId);
+            placedetails(rs, placeList);
             // log.info(placeList.toString());
             return placeList;
         } catch (SQLException ex) {
@@ -49,7 +54,7 @@ public class DbService {
         }
     }
 
-    private void placedetails(ResultSet rs, ArrayList<PlaceEntity> list2, String uuid)
+    private void placedetails(ResultSet rs, ArrayList<PlaceEntity> list2)
             throws SQLException {
         while (rs.next()) {
             try {
@@ -62,7 +67,12 @@ public class DbService {
                         rs.getString("state")));
                 // log.info(rs.toString());
                 // uuid
-                kafkaProducerService.placeSearchData(rs, uuid);
+                UUID uuid1 = UUID.randomUUID();
+                String PREFIX = "TRV_EXP_SR_ID";
+                String timeStamp = new SimpleDateFormat("***yyyy**MM**dd**-HH**mm**ss***").format(new Date());
+
+                String searchId = PREFIX + " - " + uuid1.toString() + " - " + timeStamp;
+                kafkaProducerService.placeSearchData(rs, searchId);
 
             } catch (Exception e) {
                 log.error("error", e);
